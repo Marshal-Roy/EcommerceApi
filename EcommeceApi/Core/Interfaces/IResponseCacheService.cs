b41bc394ab2908916082601668d505d3f63d6844
Marshal-Roy <marshalroy911@gmail.com>
2024-05-09 10:57:20 +0530

    Ecommerce

---
+++ EcommeceApi/Core/Interfaces/IResponseCacheService.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Core.Interfaces
+{
+    public interface IResponseCacheService
+    {
+        Task CacheResponseAsync(string chacheKey, object response, TimeSpan timeToLive);
+        Task<string> GetCachedResponseAsync(String cacheKey);
+    }
+}
